655843a03fe9e4d43a9009173238928dc85c3578
ehoversten <ehoversten@gmail.com>
2020-11-23 20:42:43 -0500

    initial setup commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// CSS Imports
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
