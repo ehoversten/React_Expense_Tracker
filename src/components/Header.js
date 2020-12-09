85a841ff3708e30055601552dee277b71d4340f4
ehoversten <ehoversten@gmail.com>
2020-12-09 12:43:46 -0500

    added react-bootstrap Navbar styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div className="container mt-5">
-            <h2>React Expense Tracker utilzing Hooks and ContextAPI</h2>
-        </div>
+        <Navbar bg="dark" variant="dark" className="mb-5">
+            <Navbar.Brand href="#home">React Expense Tracker </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text>utilizing Hooks and ContextAPI</Navbar.Text>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
